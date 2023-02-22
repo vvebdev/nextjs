69c08738e2158df6599d7b62e293bfbb26e96d80
ivanstrilets <45265680+ivanstrilets@users.noreply.github.com>
2023-02-22 17:09:46 +0000

    Deploying to gh-pages from @ vvebdev/vvebdev-nextjs@df0a18e8323538a99933f206836f17ce0f54c21b 🚀

--- _next/static/2yreUmuzeCm5s03kAnZGV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/77e878d72d98d937.css","static/chunks/pages/index-1a68ab4f2ecf2a7a.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
