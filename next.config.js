df0a18e8323538a99933f206836f17ce0f54c21b
Ivan Strilets <i.strilets@vk.team>
2023-02-22 20:09:07 +0300

    images source update

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // images: {
-  //   loader: 'imgix',
-  //   path: '',
-  // },
   images: {
     loader: 'akamai',
     path: '/',
   },
-  // basePath: '/vvebdev-nextjs',
-  // basePath: '/nextjs-pages',
-  // assetPrefix: '/nextjs-pages',
+  basePath: '/vvebdev-nextjs',
+  assetPrefix: '/vvebdev-nextjs',
 };
 
 module.exports = nextConfig;
