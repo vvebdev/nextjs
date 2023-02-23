956198937477c51a2ad30e20fb25c7aa8f7c54c7
Ivan Strilets <i.strilets@vk.team>
2023-02-23 17:01:51 +0300

    add styled component

--- src/layouts/layout-default/index.jsx
+++ src/layouts/layout-default/index.jsx
@@ -1,23 +1,27 @@
 import Head from 'next/head';
+import styled from 'styled-components';
 
-export default function LayoutDefault({ title, description, children }) {
+const Main = styled.main`
+  padding: 16px;
+`;
+
+export default function LayoutDefault({
+  title,
+  description,
+  children,
+}) {
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta
-          name="description"
-          content={description}
-        />
+        <meta name="description" content={description} />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1"
         />
         <link rel="icon" href="/nextjs/favicon.ico" />
       </Head>
-      <main className='layout-default'>
-        {children}
-      </main>
+      <Main>{children}</Main>
     </>
   );
 }
