21a774799774b3c623e1d8da2443fbd2952df9f7
Ivan Strilets <i.strilets@vk.team>
2023-03-22 15:23:53 +0300

    iphone bug test

---
+++ src/pages/bugtest.jsx
@@ -0,0 +1,41 @@
+
+import { useRef } from 'react';
+import styled from 'styled-components';
+
+
+const Wrapper = styled.main`
+  position: fixed;
+  inset: 0;
+  padding: 16px;
+`;
+
+const Inner = styled.main`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: lightblue;
+`;
+
+export default function HomePage() {
+
+  const inputRef = useRef();
+
+  setTimeout(() => {
+    inputRef.current?.focus();
+  }, 1000)
+
+  return (
+    <Wrapper>
+      <Inner>
+        <br />
+        <br />
+        <input ref={inputRef} />
+        <br />
+        <br />
+        <button>i am button</button>
+      </Inner>
+    </Wrapper>
+  );
+}
