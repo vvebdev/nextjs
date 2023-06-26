87223544dad8ae50e5b3d7b411ce2521431413b0
Ivan Strilets <i.strilets@vk.team>
2023-06-26 23:26:41 +0300

    update config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  reactStrictMode: true,
+  // reactStrictMode: true,
   images: {
     loader: "custom",
     loaderFile: "utils/image-loader.ts",
   },
-  basePath: "/nextjs/",
-  assetPrefix: "/nextjs/",
+  basePath: "/nextjs",
+  assetPrefix: "/nextjs",
 };
 
 module.exports = nextConfig;
