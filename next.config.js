aadcc41d48ef9ebb57065d52ce0078ff8af3e534
Ivan Strilets <i.strilets@vk.team>
2023-06-27 00:08:13 +0300

    test link

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  // reactStrictMode: true,
+  reactStrictMode: true,
+  trailingSlash: true,
   images: {
     loader: "custom",
     loaderFile: "utils/image-loader.ts",
