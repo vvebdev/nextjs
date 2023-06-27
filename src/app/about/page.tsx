bcc9f8bf9399f9b01dc09cd17fd6f8ee931123eb
Ivan Strilets <i.strilets@vk.team>
2023-06-27 14:38:31 +0300

    add styled-components

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,17 @@
+'use client'
+
 import Link from 'next/link'
 import Image from '@/components/Image'
 
+import styled from 'styled-components'
+
+const Container = styled.main`
+  padding: 20px;
+`;
+
 export default function AboutPage() {
   return (
-    <>
+    <Container>
       <h1>About page</h1>
       <Image src="next.svg" alt="test image from root dir" width={100} height={25} />
       <br />
@@ -11,6 +19,6 @@ export default function AboutPage() {
       <div>
         <Link href={{pathname: '/'}}>to main</Link>
       </div>
-    </>
+    </Container>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+'use client'
+
 import Link from 'next/link'
 import Image from '@/components/Image'
 
+import styled from 'styled-components'
+
+const Container = styled.main`
+  padding: 20px;
+`;
+const Container2 = styled.main`
+  padding: 20px;
+`;
+
 export default function HomePage() {
   return (
-    <>
+    <Container>
       <h1>Main Page</h1>
       <Image src="next.svg" alt="test image from root dir" width={100} height={25} />
       <br />
@@ -11,6 +22,6 @@ export default function HomePage() {
       <div>
         <Link href="/about">to about</Link>
       </div>
-    </>
+    </Container>
   )
 }
