82d5e9021278afbda79cd494fb169ce1508ac8d8
Ivan Strilets <i.strilets@vk.team>
2023-07-11 22:59:28 +0300

    delete comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function HomePage() {
   return (
     <>
       <TitleStyled>Main Page</TitleStyled>
+      
       <Image src="next.svg" alt="test image from root dir" width={100} height={25} />
       <br />
       <br />
--- src/components/TitleStyled.tsx
+++ src/components/TitleStyled.tsx
@@ -1,10 +1,6 @@
 'use client'
 import styled from 'styled-components'
 
-// interface TitleProps {
-//   text: string
-// }
-
 const Title = styled.h1`
   color: red;
 `
