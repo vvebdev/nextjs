464ab36168648f3639542bb1b68dfb37a0633f48
ivanstrilets <45265680+ivanstrilets@users.noreply.github.com>
2023-07-11 20:34:12 +0000

    Deploying to gh-pages from @ vvebdev/nextjs@3706c8cdda63bd479b8d9be0a0a533eac870e35d 🚀

---
+++ _next/static/chunks/app/user/[id]/page-ad7c4ae05cd9971a.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[805],{5501:function(e,r,n){Promise.resolve().then(n.bind(n,9125))},9125:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return o}});var t=n(9268);function o(e){let{params:r}=e;return(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)("h1",{children:"User page"}),(0,t.jsxs)("p",{children:["user id = ",r.id]})]})}},3177:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(6006),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,f=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function c(e,r,n){var t,s={},c=null,_=null;for(t in void 0!==n&&(c=""+n),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(_=r.ref),r)u.call(r,t)&&!i.hasOwnProperty(t)&&(s[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===s[t]&&(s[t]=r[t]);return{$$typeof:o,type:e,key:c,ref:_,props:s,_owner:f.current}}r.Fragment=s,r.jsx=c,r.jsxs=c},9268:function(e,r,n){"use strict";e.exports=n(3177)}},function(e){e.O(0,[253,769,744],function(){return e(e.s=5501)}),_N_E=e.O()}]);
