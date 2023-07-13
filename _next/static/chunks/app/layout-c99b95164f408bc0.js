cfc0a91a28f1e5ee3cd45bf138732ae71189d209
ivanstrilets <45265680+ivanstrilets@users.noreply.github.com>
2023-07-13 15:15:58 +0000

    Deploying to gh-pages from @ vvebdev/nextjs@2ea63d0d422e3856a7046de563f864df422ee00f 🚀

---
+++ _next/static/chunks/app/layout-c99b95164f408bc0.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7751:function(e,t,n){Promise.resolve().then(n.bind(n,4484)),Promise.resolve().then(n.t.bind(n,4270,23)),Promise.resolve().then(n.t.bind(n,8824,23))},4484:function(e,t,n){"use strict";n.r(t),n.d(t,{default:function(){return u}});var r=n(9268),o=n(6006),s=n(6008),f=n(8945);function u(e){let{children:t}=e,[n]=(0,o.useState)(()=>new f.qH);return(0,s.useServerInsertedHTML)(()=>{let e=n.getStyleElement();return n.instance.clearTag(),e}),(0,r.jsx)(r.Fragment,{children:t})}},8824:function(){},4270:function(e){e.exports={style:{fontFamily:"'__Inter_f113a6', '__Inter_Fallback_f113a6'",fontStyle:"normal"},className:"__className_f113a6"}},3177:function(e,t,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var r=n(6006),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),f=Object.prototype.hasOwnProperty,u=r.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,a={key:!0,ref:!0,__self:!0,__source:!0};function l(e,t,n){var r,s={},l=null,_=null;for(r in void 0!==n&&(l=""+n),void 0!==t.key&&(l=""+t.key),void 0!==t.ref&&(_=t.ref),t)f.call(t,r)&&!a.hasOwnProperty(r)&&(s[r]=t[r]);if(e&&e.defaultProps)for(r in t=e.defaultProps)void 0===s[r]&&(s[r]=t[r]);return{$$typeof:o,type:e,key:l,ref:_,props:s,_owner:u.current}}t.Fragment=s,t.jsx=l,t.jsxs=l},9268:function(e,t,n){"use strict";e.exports=n(3177)},6008:function(e,t,n){e.exports=n(794)}},function(e){e.O(0,[945,253,769,744],function(){return e(e.s=7751)}),_N_E=e.O()}]);
